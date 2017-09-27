dba786432b3904616c7b323e7ba9606841a145d7
Neil Lee <neil.lee@udngroup.com.tw>
2017-09-27 19:28:12 +0800

    Create the 'App' component to act as the root component for the application

--- ageteller/src/index.js
+++ ageteller/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App';
+
 ReactDOM.render(
-  <div>
-    <h2>AgeTeller Application</h2>
-  </div>,
+  <App />,
   document.getElementById('root')
 );
