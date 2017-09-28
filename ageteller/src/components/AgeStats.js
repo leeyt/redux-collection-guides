6e0724d9025fa4eb70e93a0c40de10adb2b71cef
Neil Lee <neil.lee@udngroup.com.tw>
2017-09-28 13:03:55 +0800

    Compute the age stats and display it

--- ageteller/src/components/AgeStats.js
+++ ageteller/src/components/AgeStats.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
 class AgeStats extends Component {
+  timeSince(date) {
+    let today_ms = new Date().getTime();
+    let date_ms = new Date(date).getTime();
+    let difference = Math.abs(today_ms - date_ms);
+
+    let days = Math.floor(difference / (1000*3600*24));
+    let years = Math.floor(days / 365);
+    days -= years * 365;
+    let months = Math.floor(days / 31);
+    days -= months * 31;
+
+    return `${years} years, ${months} months, ${days} days`;
+  }
+
   render() {
-    console.log(this);
     return (
-      <div>{this.props.date}</div>
+      <div>
+        <h4>Congrats on staying alive for {this.timeSince(this.props.date)} on this planet.</h4>
+      </div>
     )
   }
 }
