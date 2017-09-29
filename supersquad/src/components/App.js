38cc4214f810f30758b13b6008daa461e4bb8521
Neil Lee <neil.lee@udngroup.com.tw>
2017-09-29 19:02:51 +0800

    Create a 'HeroList' component for displaying the heroes selected for super squad.

--- supersquad/src/components/App.js
+++ supersquad/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import CharacterList from './CharacterList';
+import HeroList from './HeroList';
 
 import '../styles/index.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
       <div className="App">
         <h2>SuperSquad</h2>
         <CharacterList />
+        <HeroList />
       </div>
     )
   }
---
+++ supersquad/src/components/HeroList.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class HeroList extends Component {
+  render() {
+    let { heroes } = this.props;
+
+    return (
+      <div>
+        <h4>Heroes</h4>
+        <ul className="list-group">
+        {
+          heroes.map(hero => {
+            return (
+              <li
+                key={hero.id}
+                className="list-group-item"
+              >
+                <div
+                  className="list-item"
+                >
+                  {hero.name}
+                </div>
+                <div
+                  className="list-item right-button"
+                >
+                  x
+                </div>
+              </li>
+            )
+          })
+        }
+        </ul>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    heroes: state.heroes
+  };
+}
+
+export default connect(mapStateToProps, null)(HeroList);
