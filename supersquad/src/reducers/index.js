872191b022dc4ec3697b79115223c02c355c66c3
Neil Lee <neil.lee@udngroup.com.tw>
2017-09-29 14:31:49 +0800

    Create a second reducer to keep track of the heroes team, and combine it with the characters reducer to form the root reducer.

--- supersquad/src/reducers/index.js
+++ supersquad/src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from 'redux';
+
 import characters_json from '../data/characters.json';
 
 import { ADD_CHARACTER } from '../actions';
@@ -12,4 +14,21 @@ function characters(state = characters_json, action) {
   }
 }
 
-export default characters;
+function heroes(state = [], action) {
+  switch(action.type) {
+  case ADD_CHARACTER:
+    let heroes = [ ...state, createCharacter(action.id) ];
+    return heroes;
+  default:
+    return state;
+  }
+}
+
+function createCharacter(id) {
+  let character = characters_json.find(c => c.id === id);
+  return character;
+}
+
+const rootReducer = combineReducers({ characters, heroes });
+
+export default rootReducer;
