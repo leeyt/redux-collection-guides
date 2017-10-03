48ff66451c5f08dec4160f845f33f9d174c47548
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-03 15:02:10 +0800

    Search recipes by calling recipepuppy's API

--- recipefinder/src/components/SearchRecipes.js
+++ recipefinder/src/components/SearchRecipes.js
@@ -15,7 +15,9 @@ class SearchRecipes extends Component {
     let { ingredients, dish } = this.state;
     const url = `http://www.recipepuppy.com/api/?i=${ingredients}&q=${dish}`;    
 
-    console.log('url', url);
+    fetch(url, { method: 'GET' })
+      .then(response => response.json())
+      .then(json => console.log('recipes', json));
   }
 
   render() {
