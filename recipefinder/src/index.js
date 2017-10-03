bc7c193ae2c8f8e34de6a3e437d8df1d658e31a5
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-03 15:21:11 +0800

    Create a store and provide it to the App component

--- recipefinder/src/index.js
+++ recipefinder/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
