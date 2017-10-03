74441e501ef9f9e26d3b62f8ff844d6670c77aa5
Neil Lee <neil.lee@udngroup.com.tw>
2017-10-03 12:16:10 +0800

    Add custom styling for centering and extra padding.

--- recipefinder/src/components/App.js
+++ recipefinder/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 import SearchRecipes from './SearchRecipes';
 
+import '../styles/index.css';
+
 class App extends Component {
   render() {
     return (
